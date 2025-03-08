211302c2ebacdb952ccf4905a7cbcf5c7908139d
zyad ahmed <zezozoz131@gmail.com>
2025-03-08 22:49:21 +0200

    Add files via upload

---
+++ q1.dart
@@ -0,0 +1,8 @@
+void main() {
+  int n = 5;
+  if (n % 2 == 0) {
+    print("Even");
+  } else {
+    print("Odd");
+  }
+}
---
+++ q3.dart
@@ -0,0 +1,26 @@
+void main() {
+  int n = 1;
+  switch (n) {
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    case 4:
+      print("Thursday");
+      break;
+    case 5:
+      print("Friday");
+      break;
+    case 6:
+      print("Saturday");
+      break;
+    case 7:
+      print("Sunday");
+      break;
+  }
+}
